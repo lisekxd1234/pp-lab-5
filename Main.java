629fdfe8dbfa41ae4cb1f1a33daa3cfc97a15b31
lisekxd1234 <lisekxd1234@gmail.com>
2024-05-04 12:21:13 +0200

    Dodano klasę MathUtils oraz zaktualizowano metodę main w klasie Main

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        // w metodzie main utwórz instancję EmailMessenger i użyj metody sendMessage
+        // dodaj dwie liczby za pomocą metody add z klasy MathUtils
+        int liczba1 = 10;
+        int liczba2 = 5;
+        int suma = MathUtils.add(liczba1, liczba2);
+        
+        // wyświetl wynik dodawania
+        System.out.println("Wynik dodawania: " + suma);
+        
+        // utwórz instancję EmailMessenger i użyj metody sendMessage
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("To jest nowa wiadomosc email");
+        messenger.sendMessage("Wynik dodawania: " + suma);
     }
 }
---
+++ company/utils/MathUtils.java
@@ -0,0 +1,8 @@
+package company.utils;
+
+public class MathUtils {
+    // dodaj statyczną metodę add(int a, int b), która zwraca sumę dwóch liczb
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
