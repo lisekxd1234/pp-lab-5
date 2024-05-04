6223ae0f10b2d54df382caa05ae4a7f70731d5d9
lisekxd1234 <lisekxd1234@gmail.com>
2024-05-04 12:29:56 +0200

    Zaktualizowano metodę main w klasie Main zgodnie z wymaganiami zadania 4

--- Main.java
+++ Main.java
@@ -4,16 +4,31 @@ import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        // dodaj dwie liczby za pomocą metody add z klasy MathUtils
-        int liczba1 = 10;
-        int liczba2 = 5;
-        int suma = MathUtils.add(liczba1, liczba2);
+        // utwórz tablicę people z pięcioma obiektami instancji Person
+        Person[] people = new Person[5];
         
-        // wyświetl wynik dodawania
-        System.out.println("Wynik dodawania: " + suma);
+        // utwórz stałą b o wartości 10
+        final int b = 10;
         
-        // utwórz instancję EmailMessenger i użyj metody sendMessage
-        Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Wynik dodawania: " + suma);
+        // w bloku try-catch przypisz dla każdej instancji nową instancję Person
+        try {
+            people[0] = new Person("Jan Kowalski", 30);
+            people[1] = new Person("Anna Nowak", 25);
+            people[2] = new Person("Marek Wiśniewski", 35);
+            people[3] = new Person("Katarzyna Dąbrowska", 40);
+            people[4] = new Person("Piotr Lewandowski", 45);
+        } catch (InvalidAgeException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        }
+        
+        // wylicz dla każdej osoby z osobna wartość za pomocą metody add z MathUtils
+        // wykorzystując jako argument a wiek danej osoby a argument b, utworzoną wcześniej stałą
+        for (Person person : people) {
+            int suma = MathUtils.add(person.getAge(), b);
+            
+            // wykorzystaj wyliczoną wartość w treści wiadomości metody sendMessage
+            Messenger messenger = new EmailMessenger();
+            messenger.sendMessage("Wynik dodawania wieku " + person.getName() + " (" + person.getAge() + ") i " + b + " to: " + suma);
+        }
     }
 }
